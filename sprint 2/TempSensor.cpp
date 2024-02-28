6ee2f5d9a7e31ce77902f7f2b2747ec13bc3bd3f
unknown <02fe21bcs005@klescet.ac.in>
2024-02-28 17:24:04 +0530

    sprint1

--- sprint 2/TempSensor.cpp
+++
@@ -1,7 +0,0 @@
-// TemperatureSensor.cpp
-#include "TemperatureSensor.h"
-
-double TemperatureSensor::getCurrentTemperature() const {
-    // For simplicity, returning a constant value; in a real scenario, you would obtain this from a sensor.
-    return 15.0;
-}
