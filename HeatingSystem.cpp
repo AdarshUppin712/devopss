918ccb5ea90cbd27bfe8ba10398c06a180156789
AdarshUppin712 <161579906+AdarshUppin712@users.noreply.github.com>
2024-02-28 16:41:26 +0530

    Delete HeatingSystem.cpp

--- HeatingSystem.cpp
+++
@@ -1,7 +0,0 @@
-// HeatingSystem.cpp
-#include "HeatingSystem.h"
-
-double HeatingSystem::calculateRequiredTemperature(double currentTemperature) const {
-    // For simplicity, returning a constant value; in a real scenario, you would calculate based on various factors.
-    return currentTemperature + 5.0;
-}
